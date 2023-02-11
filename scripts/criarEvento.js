e3d54c78806bf3535383f73105351f3bd18739d9
guipaterlini <guilhermepaterlini@gmail.com>
2023-02-11 12:01:33 -0400

    Feat: adicionado função para enviar dados do formulario de novo evento para a api

--- scripts/criarEvento.js
+++ scripts/criarEvento.js
@@ -1,5 +1,6 @@
 var formNewEvent = document.querySelector("#form-new-event");
 
+//Objeto contendo as informações do formulario
 const newEvent = {};
 
 formNewEvent.addEventListener("submit", function (event) {
@@ -7,6 +8,7 @@ formNewEvent.addEventListener("submit", function (event) {
 
   const inputs = formNewEvent.elements;
 
+  // Passa pelo campos do formulario e coleta as informações colocando-as no newEvent Object
   for (let i = 0; i < inputs.length; i++) {
     if (inputs[i].type !== "submit") {
       newEvent[inputs[i].name] = inputs[i].value;
@@ -14,3 +16,23 @@ formNewEvent.addEventListener("submit", function (event) {
   }
   console.log(newEvent);
 });
+
+console.log(JSON.stringify(newEvent));
+
+//Enviando dados para a API
+const apiUrl = "soundgarden-api.deta.dev/events";
+
+fetch(apiUrl, {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify(newEvent),
+})
+  .then((reponse) => response.json())
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
