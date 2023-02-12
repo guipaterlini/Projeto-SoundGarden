1565dacf22348d3817f17b3b84bb0284c6088922
guipaterlini <guilhermepaterlini@gmail.com>
2023-02-11 21:07:04 -0400

    Feat: adicionada validação para quando algum campo do formulario estiver vazio

--- scripts/criarEvento.js
+++ scripts/criarEvento.js
@@ -14,6 +14,12 @@ formNewEvent.addEventListener("submit", function (event) {
   // Passa pelo campos do formulario e coleta as informações colocando-as no newEvent Object
   for (let i = 0; i < inputs.length; i++) {
     if (inputs[i].type !== "submit") {
+      // validando se o campo está vazio
+      if (!inputs[i].value) {
+        alert("Por favor preencha todos os campos.");
+        return;
+      }
+
       switch (inputs[i].name) {
         case "attractions":
           // Verifica se o input name é atracoes e se for transforma o conteudo dele em uma array
