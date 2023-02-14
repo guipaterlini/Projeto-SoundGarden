a4758615f7cd3be1731d3eb23cf2577fae853ccf
guipaterlini <guilhermepaterlini@gmail.com>
2023-02-13 22:05:14 -0400

    Feat: concluido a o fetch que puxa os dados de um evento usando o ID

--- scripts/excluirEvento.js
+++ scripts/excluirEvento.js
@@ -4,22 +4,23 @@ const searchParams = new URLSearchParams(url.search);
 const id = searchParams.get("id");
 
 const preencherFormExcluirEvento = function (data) {
-
-}
+  const inputName = document.querySelector('nome');
+  inputName.value = data.name;
+};
 
 // Busca na api pelos dados do event a ser excluido
-const apiUrl = "https://soundgarden-api.deta.dev/events";
-  
-  fetch(apiUrl+id, {
-    method: 'GET',
-    redirect: 'follow'
-  })
-    .then(response => response.json())
-    .then(data => preencherFormExcluirEvento(data))
-    .catch(error => console.log('error', error));
+const apiUrl = "https://soundgarden-api.deta.dev/events/";
 
-
-
+fetch(apiUrl + id, {
+  method: "GET",
+  redirect: "follow",
+  headers: {
+    "Content-Type": "application/json",
+  },
+})
+  .then((response) => response.json())
+  .then((data) => console.log(data))
+  .catch((error) => console.log("error", error));
 
 // fetch get com id
 
