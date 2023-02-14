630fce6b9111beaba64604770ac5c63ffb8f52a8
guipaterlini <guilhermepaterlini@gmail.com>
2023-02-13 22:42:11 -0400

    Feat: função que preenche o formuario da pagina de exclusão d evento

--- scripts/excluirEvento.js
+++ scripts/excluirEvento.js
@@ -3,27 +3,35 @@ const url = new URL(window.location.href);
 const searchParams = new URLSearchParams(url.search);
 const id = searchParams.get("id");
 
+// Preenche o formulario com os dados que vem da api
 const preencherFormExcluirEvento = function (data) {
-  const inputName = document.querySelector('nome');
-  inputName.value = data.name;
+  // Para deixar o codigo mais curto eu usei 'desestruturação de objetos'
+  const { name, banner, attractions, description, scheduled, number_tickets } =
+    data;
+  document.querySelector("#nome").value = name;
+  document.querySelector("#banner").value = banner;
+  document.querySelector("#atracoes").value = attractions;
+  document.querySelector("#descricao").value = description;
+  document.querySelector("#data").value = scheduled;
+  document.querySelector("#lotacao").value = number_tickets;
 };
 
 // Busca na api pelos dados do event a ser excluido
 const apiUrl = "https://soundgarden-api.deta.dev/events/";
 
-fetch(apiUrl + id, {
-  method: "GET",
-  redirect: "follow",
-  headers: {
-    "Content-Type": "application/json",
-  },
-})
-  .then((response) => response.json())
-  .then((data) => console.log(data))
-  .catch((error) => console.log("error", error));
+const getByIdExcluirDados = function (event) {
+  fetch(apiUrl + id, {
+    method: "GET",
+    redirect: "follow",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => preencherFormExcluirEvento(data))
+    .catch((error) => console.log("error", error));
+};
 
-// fetch get com id
-
-// preencher formulario com informções da api
+getByIdExcluirDados();
 
 // no submit excluir o evento
