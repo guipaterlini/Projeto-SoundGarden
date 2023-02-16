de793a7c63d51766e213b7996322a21d1522796a
guipaterlini <guilhermepaterlini@gmail.com>
2023-02-15 23:18:02 -0400

    Feat: ajustado editar evento para que ele leia o formulario usando a função lerFormulario

--- scripts/editarEvento.js
+++ scripts/editarEvento.js
@@ -1,5 +1,6 @@
 import { formataDataToLocal } from "./utils/formataDataToLocal.js";
 import { formataDataISO8601 } from "./utils/formataDataISO8601.js";
+import { lerFormulario } from "./utils/lerFormulario.js";
 
 const endpoint = "https://soundgarden-api.vercel.app/events/";
 
@@ -45,45 +46,7 @@ formEditarEvento.addEventListener("submit", function (event) {
 
   const inputs = formEditarEvento.elements;
 
-  // Passa pelo campos do formulario e coleta as informações colocando-as no newEvent Object
-  for (let i = 0; i < inputs.length; i++) {
-    if (inputs[i].type !== "submit") {
-      // validando se o campo está vazio
-      if (!inputs[i].value) {
-        alert("Por favor preencha todos os campos.");
-        return;
-      }
-
-      switch (inputs[i].name) {
-        case "attractions":
-          // Verifica se o input name é atracoes e se for transforma o conteudo dele em uma array
-          eventoEditado[inputs[i].name] = inputs[i].value.split(/\s*,\s*/);
-          break;
-        case "number_tickets":
-          // Verifica se o input name é number_tickets e se for transforma o conteudo dele em um number
-          eventoEditado[inputs[i].name] = parseInt(inputs[i].value);
-          break;
-        case "scheduled":
-          // Verifica se o input name é scheduled e se for transforma o conteudo dele em uma data formato ISO 8601
-
-          // Checa se o valor do input vem no formato DD/MM/YYYY HH:mm
-          let editadoEventDate = inputs[i].value;
-          let regEx = /^\d{2}\/\d{2}\/\d{4} \d{2}:\d{2}$/;
-
-          if (!editadoEventDate.match(regEx)) {
-            alert(
-              "Data inválida. Por favor, insira uma data e horário no formato 00/00/0000 00:00."
-            );
-            return;
-          }
-          eventoEditado[inputs[i].name] = formataDataISO8601(editadoEventDate);
-          break;
-        default:
-          // caso seja de outro tipo, só acrescenta sem nenhum tratamento
-          eventoEditado[inputs[i].name] = inputs[i].value;
-      }
-    }
-  }
+  lerFormulario(inputs, eventoEditado);
 
   fetch(endpoint + id, {
     method: "PUT",
