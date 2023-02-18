6036ffa6a85a8578cd1154a92ccc390ed285a972
guipaterlini <guilhermepaterlini@gmail.com>
2023-02-18 00:21:47 -0400

    Feat: adicionado o html para o modal na pagina de eventos

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-// conferir a conexão com o banco de dados
-include_once 'conexão.php';
-
-$nome = filter_input(INPUT_POST, 'nome', FILTER_SANATIZE_STRING);
-$email = filter_input(INPUT_POST, 'email', FILTER_SANATIZE_STRING);
-
-echo "Nome: " . $nome . "<br>";
-echo "Email: " . $email . "<br>";
