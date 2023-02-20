3ec8a005f097ffccadcad2d93c262f76b1b81f05
guipaterlini <guilhermepaterlini@gmail.com>
2023-02-20 00:53:31 -0400

    Fix: ajustado para que ao se abrir e fechar o modal varias vezes nao se crie multiplas reservas

--- scripts/criarReserva.js
+++ scripts/criarReserva.js
@@ -1,32 +1,27 @@
 import { endpointReserva } from "./utils/apiEndpoint.js";
 import { lerFormulario } from "./utils/lerFormulario.js";
 
-export const criarReserva = function (idEvento) {
+export const criarReserva = function (idEvento, objectReserva) {
   var formNovaRserva = document.querySelector("#form-reservar");
-  //Objeto contendo as informações do formulario
-  const objectReserva = {};
 
-  formNovaRserva.addEventListener("submit", function (event) {
-    event.preventDefault();
+  const inputs = formNovaRserva.elements;
+  // função que le os dados do formulario
+  lerFormulario(inputs, objectReserva, idEvento);
 
-    const inputs = formNovaRserva.elements;
-    // função que le os dados do formulario
-    lerFormulario(inputs, objectReserva, idEvento);
-
-    // função que envia os dados pela api
-    fetch(endpointReserva, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(objectReserva),
+  // função que envia os dados pela api
+  fetch(endpointReserva, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(objectReserva),
+  })
+    .then((response) => response.json())
+    .then(() => {
+      alert("Reserva feita com sucesso!");
+      console.log(objectReserva);
     })
-      .then((response) => response.json())
-      .then(() => {
-        alert("Reserva feita com sucesso!");
-      })
-      .catch((error) => {
-        console.error("Erro ao processar a resposta do servidor: ", error);
-      });
-  });
+    .catch((error) => {
+      console.error("Erro ao processar a resposta do servidor: ", error);
+    });
 };
